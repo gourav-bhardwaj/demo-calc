62f30022306fe0b80d177c9934f3c9761fb3e1e5
GouravKumar <gov.kumarbharatdwaj@gmail.com>
2025-05-28 16:30:42 +0530

    Implement a separate service class for SRP

---
+++ DemoService.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class DemoService {
+
+   public void arithOperate(){
+	   Integer n1=0, n2=0, op=0;
+	   Scanner sc=new Scanner(System.in);
+	   
+	   System.out.println("Enter first number - 1::");
+       n1=sc.nextInt();
+	   
+	   AGAIN:
+	   while(true){
+		   System.out.println("Select any one operator name :: \n1. ADD\n2. SUBTRACT\n3. MULTIPLY\n4. DIVIDE");
+		   op=sc.nextInt();
+		   
+		   System.out.println("Enter second number");
+           n2=sc.nextInt();
+		   
+		   switch(op){
+			   case 1:
+			     n1=n1+n2;
+			   break;
+			   case 2:
+			     n1=n1-n2;
+			   break;
+			   case 3:
+			     n1=n1*n2;
+			   break;
+			   case 4:
+			     n1=n1/n2;
+			   break;
+			   default:
+			   System.out.println("invalid operator!!");
+			   continue AGAIN;
+		   }  
+		   
+		   
+		   System.out.println("Result :: "+n1);
+		   
+	   }
+	   
+   }
+  
+}
