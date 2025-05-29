f4270dc35c389b937779aa7ff26caec2c5c3fa73
GouravKumar <gov.kumarbharatdwaj@gmail.com>
2025-05-29 21:14:28 +0530

    Fix as per code review feedbacks

--- DemoCalc.java
+++ DemoCalc.java
@@ -7,12 +7,11 @@ class DemoCalc {
        obj.printTable(5);
    }
 
-
-    public void printMultiplicationTable(int n) {
-        IntStream.rangeClosed(1, 10)
-                .mapToObj(i -> "%d x %d = %d".formatted(n, i, (n * i)))
-                .forEach(System.out::println);
-    }
+   public void printTable(int n) {
+       for (int i = 1; i <= 10; i++) {
+           System.out.println("%d x %d = %d".formatted(n, i, (n*i)));
+       }
+   }
 
    public int uiuigjbbubu() {
         Integer n1=0, n2=0, op=0;
